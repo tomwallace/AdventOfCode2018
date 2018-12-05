4ae003a905bd02f9bf708ad7b1021dc5bb5f7505
Tom Wallace <thomaspwallace@gmail.com>
2018-12-05 09:46:04 -0500

    Added Day Five puzzles and tests.  Existing solution is not performant, so tests are commented out.  Left a TODO to fix.

---
+++ AdventOfCode2018.Tests/DayFiveTests.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode2018.Five;
+using Xunit;
+
+namespace AdventOfCode2018.Tests
+{
+    public class DayFiveTests
+    {
+        [Fact]
+        public void CalculateUnitsRemainingAfterReactions()
+        {
+            var input = "dabAcCaCBAcCcaDA";
+            var sut = new DayFive();
+            var result = sut.CalculateUnitsRemainingAfterReactions(input);
+
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void FindBestPolymerAfterRemovingAUnit()
+        {
+            var input = "dabAcCaCBAcCcaDA";
+            var sut = new DayFive();
+            var result = sut.FindBestPolymerAfterRemovingAUnit(input);
+
+            Assert.Equal(4, result);
+        }
+
+        // TODO: Figure out how to change the process to make faster
+        // Takes a minute to run, so commenting out
+        /*
+        [Fact]
+        public void PartA_Actual()
+        {
+            var sut = new DayFive();
+            var result = sut.PartA();
+
+            // 27612 is too high
+            Assert.Equal("10250", result);
+        }
+        */
+
+        // Takes 17 minutes to run, so commenting out
+        /*
+        [Fact]
+        public void PartB_Actual()
+        {
+            var sut = new DayFive();
+            var result = sut.PartB();
+
+            Assert.Equal("6188", result);
+        }
+        */
+    }
+}
